f5d837e37a59f0a35bf8dd357dcb54379bfcc675
Roxbili <roxbili@163.com>
2022-01-07 12:50:46 +0800

    添加了最新的timm脚本

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 ## 目录介绍
 
+### 根目录
+- main.py: 旧训练脚本，timm==0.3.4
+- train.py: 适配最新timm的训练脚本
+
 ### models
 - statistic.py: 用于统计推理内存的类
 - t2t_vit.py: t2t模型基本结构
@@ -9,9 +13,10 @@
 
 ## 运行方式
 ```bash
-bash script/train.py
+bash script/train.sh
 ```
 
-从磁盘导入数据集实在是太慢了，导致GPU利用率一直是0。
-因此`list(loader)`强行提前将整个数据集加载进内存中(no-prefetcher需要打开)。
+[问题发现] 从磁盘导入数据集实在是太慢了，导致GPU利用率一直是0。
+
+[解决方案] `list(loader)`强行提前将整个数据集加载进内存中(no-prefetcher需要打开)。
 目前224大小的数据集是不需要的，改成144缩小一下吧，减小内存占用。
--- script/train.sh
+++ script/train.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-# python main.py /home/fanding/dataset/ILSVRC2012 --model t2t_vit_7 -b 128 --lr 1e-3 --weight-decay .03 --img-size 224
+# SCRIPT_NAME="main.py"
+SCRIPT_NAME="train.py"
+
+# python ${SCRIPT_NAME} /home/fanding/dataset/ILSVRC2012 --model t2t_vit_7 -b 128 --lr 1e-3 --weight-decay .03 --img-size 224
 
 # my model for converting to tflite
-# python main.py /home/fanding/dataset/ILSVRC2012 --model t2t_vit_t_1 -b 32 --lr 1e-3 --weight-decay .03 --img-size 224 --no-prefetcher
-# python main.py /home/fanding/dataset/ILSVRC2012 --model test_model -b 32 --lr 1e-3 --weight-decay .03 --img-size 224
-# python main.py /home/fanding/dataset/ILSVRC2012 --model search_model -b 2048 --lr 1e-3 --weight-decay .03 --epochs 100 --workers 4 --pin-mem --search --no-prefetcher
-# python main.py /home/LAB/leifd/dataset/ILSVRC2012 --model t2t_vit_t_1 -b 128 --lr 1e-3 --weight-decay .03 --img-size 224
-python main.py /home/LAB/leifd/dataset/ILSVRC2012 --model search_model -b 1024 --lr 1e-3 --weight-decay .03 --epochs 300 --workers 4 --pin-mem --search --no-prefetcher
+## za pc
+# python ${SCRIPT_NAME} /home/fanding/dataset/ILSVRC2012 --model t2t_vit_t_1 -b 32 --lr 1e-3 --weight-decay .03 --img-size 224 --no-prefetcher
+# python ${SCRIPT_NAME} /home/fanding/dataset/ILSVRC2012 --model test_model -b 32 --lr 1e-3 --weight-decay .03 --img-size 224
+# python ${SCRIPT_NAME} /home/fanding/dataset/ILSVRC2012 --model search_model -b 1024 --lr 1e-3 --weight-decay .03 --epochs 300 --workers 4 --pin-mem --search
+# python ${SCRIPT_NAME} /home/fanding/dataset/ILSVRC2012 --model search_model -b 1024 --lr 1e-3 --weight-decay .03 --epochs 300 --workers 4 --pin-mem --search --no-prefetcher
+
+## act
+# python ${SCRIPT_NAME} /home/LAB/leifd/dataset/ILSVRC2012 --model t2t_vit_t_1 -b 128 --lr 1e-3 --weight-decay .03 --img-size 224
+python ${SCRIPT_NAME} /home/LAB/leifd/dataset/ILSVRC2012 --model search_model -b 4096 --lr 1e-3 --weight-decay .03 --epochs 300 --workers 4 --pin-mem --search --no-prefetcher
 
 # # search model
 # nnictl create --config nas/test.yml -p 8877
